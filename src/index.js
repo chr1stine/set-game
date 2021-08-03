f57b006e75dc5f837bf7173e3ce58c6bb0ebb63f
Chris <kristina.beidina@gmail.com>
2021-08-03 13:23:53 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './style.less'
+import {render} from 'react-dom'
+import React from 'react'
+import App from './components/App'
+import {Deck} from './deck'
+import reducer from './reducer'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+// генерирование новой колоды
+const deck = new Deck()
+
+// инициализация хранилища
+const store = createStore(reducer)
+
+store.subscribe(()=>{
+    let state = store.getState()
+    if (state.collected){
+        store.dispatch({type:'set'})
+    }
+})
+
+render(
+    <Provider store={store}>
+        <App deck={deck} />
+    </Provider>,
+    document.getElementById('root')
+)
