2e1cd9cfbe4f98ea01087380420fd7d7fd28d374
chris <kristina.beidina@gmail.com>
2022-06-01 17:01:07 +0300

    edit config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,44 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ["@babel/polyfill","./src/index.js"],
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
-    },
-    devtool:"source-map",
-    module:{
-        rules:[
-            {
-                test: /\.js$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                      presets: [
-                        ["@babel/preset-env", {
-                        "useBuiltIns": "entry"
-                      }], "@babel/preset-react"
-                      ]
-                    }
-                  }
-            },
-            {
-                test: /\.less$/,
-                use: ['style-loader','css-loader','less-loader']
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader','css-loader']
-            }
-        ]
-    },
-    plugins:[
-        new HTMLWebpackPlugin({template:'./src/index.html'})
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 8081
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'entry'
+                }
+              ],
+              ['@babel/preset-react', { runtime: 'automatic' }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
-}
+  },
+  plugins: [new HTMLWebpackPlugin({ template: './src/index.html' })]
+};
